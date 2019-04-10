a9b9b0efd04c2c740518fa67380471d4925c53e0
Yovanny Montelongo <pickleyova@live.com>
2019-04-09 20:13:10 -0500

    Initial Commit; Added Reverse Linked List

--- DataStruct_CSharp_Montelongo/Program.cs
+++ DataStruct_CSharp_Montelongo/Program.cs
@@ -7,9 +7,63 @@ using System.Threading.Tasks;
 namespace DataStruct_CSharp_Montelongo {
     class Program {
         static void Main(string[] args) {
+            int inputChoice = 0;
+            LinkedList<int> numList = new LinkedList<int>();
+            numList.AddLast(1);
+            numList.AddLast(3);
+            numList.AddLast(15);
+            numList.AddLast(54);
+            numList.AddLast(48);
+            numList.AddLast(5);
 
-            Console.WriteLine("Welcome to The ");
+            do {
+                Console.WriteLine("Welcome to The Data Structure Console Application");
+                Console.Write(" 1) Linked List \n" +
+                              " 2) \n" +
+                              " 3) \n" +
+                              "-1) Exit");
+                Console.Write("\nEnter the number for the data structure you want to create: ");
+                int.TryParse(Console.ReadLine(), out inputChoice);
+
+                switch (inputChoice) {
+                    case 1:
+                        ReverseLinkedList(numList);
+                        break;
+                }
+
+                Console.Clear();
+            } while (inputChoice != -1);
 
         }
+
+        static void ReverseLinkedList (LinkedList<int> linkedList) {
+            LinkedList<int> copyList = new LinkedList<int>();
+
+            LinkedListNode<int> start = linkedList.Last;
+
+            while (start != null) {
+                copyList.AddLast(start.Value);
+                start = start.Previous;
+            }
+
+            Console.Clear();
+            Console.WriteLine("\nThe linked list currently has the values:");
+            for (int i = 0; i < copyList.Count; i++) {
+                Console.WriteLine($"Index {i}: {linkedList.ElementAt(i)}");
+            }
+
+            Console.WriteLine("\nThe linked list reversed has the values:");
+            for (int i = 0; i < copyList.Count; i++) {
+                Console.WriteLine($"Index {i}: {copyList.ElementAt(i)}");
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void FindNode() {
+            
+        }
+
     }
 }
